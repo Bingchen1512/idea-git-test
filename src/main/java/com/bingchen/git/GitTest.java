0eab3b4aeb7eae2d89afd6b125d8ed009e409555
bingcheni <949596252@qq.com>
2022-05-04 20:11:15 +0800

    idea first commit

---
+++ src/main/java/com/bingchen/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.bingchen.git;
+
+/**
+ * @Title:
+ * @Package
+ * @Description:
+ * @author: bingcheni
+ * @date:
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Git, hello!");
+    }
+
+}
