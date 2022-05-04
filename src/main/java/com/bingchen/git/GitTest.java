30363a491a8744ea74cee61ab6c5d090f262990d
bingcheni <949596252@qq.com>
2022-05-04 20:23:34 +0800

    idea third hot-fix commit

--- src/main/java/com/bingchen/git/GitTest.java
+++ src/main/java/com/bingchen/git/GitTest.java
@@ -13,6 +13,7 @@ public class GitTest {
         System.out.println("Git, hello!");
         System.out.println("idea second commit");
         System.out.println("merge hot-fix");
+        System.out.println("master modify");
     }
 
 }
